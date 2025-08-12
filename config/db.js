faf28015da65c5cdf3caea8e55e08b462a4da897
Tanuj Gupta <64638288+tanujgupta18@users.noreply.github.com>
2025-08-12 21:45:35 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,10 +2,16 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    const uri = process.env.MONGO_URI.replace(
+      "<username>",
+      process.env.MONGO_USER
+    ).replace("<password>", process.env.MONGO_PASS);
+
+    await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+
     console.log("MongoDB connected successfully");
   } catch (err) {
     console.error("MongoDB connection error:", err.message);
